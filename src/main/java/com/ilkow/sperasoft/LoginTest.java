97b84b9c2d5679a72ec0742ab23c2dd9a47c21cc
KOVCHENKO <il-kow@mail.ru>
2020-01-24 14:07:29 +0300

    init

---
+++ src/main/java/com/ilkow/sperasoft/LoginTest.java
@@ -0,0 +1,10 @@
+package com.ilkow.sperasoft;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test(priority=1)
+    void setup() {
+        System.out.println("This is setup method");
+    }
+}
